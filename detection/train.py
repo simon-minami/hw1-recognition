fix softmax, sigmoid q1 and change score_thresh for detection

detection/train.py
@@ -185,7 +185,7 @@ def main(args):
             detector,
             small_val_loader,
             val_dataset.idx_to_class,
-            score_thresh=0.7,
+            score_thresh=0.4,
             nms_thresh=0.5,
             device=DEVICE,
             dtype=torch.float32,

q1_q2_classification/utils.py
@@ -132,7 +132,7 @@ def eval_dataset_map(model, device, test_loader):
             data = data.to(device)
             output = model(data)
             
-            pred = F.softmax(output, dim=-1).cpu().numpy()
+            pred = torch.sigmoid(output).cpu().numpy()
             gt = target.cpu().numpy()
             valid = wgt.cpu().numpy()
             